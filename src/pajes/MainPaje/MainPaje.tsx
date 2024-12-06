ccaa0a047c71f8f65f831d64c9d2ba2d984148d9
дима феодотьев <feodotevdima@gmail.com>
2024-12-06 09:59:02 +0300

    a

--- src/pajes/MainPaje/MainPaje.tsx
+++ src/pajes/MainPaje/MainPaje.tsx
@@ -4,6 +4,10 @@ const SingUpUrl="https://localhost:7003/Wish";
 
 async function AddWish(present : string, price : number)
 {
+
+  const presents = ['apple', 'orange', 'banana'];
+  const prices = [1, 4, 6];
+
   let responseAdd = await fetch(SingUpUrl,
   {
     method: "POST",
@@ -14,62 +18,13 @@ async function AddWish(present : string, price : number)
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      WishList:
-        [
-          {
-            "present": "string",
-            "price": 0
-          }
-        ]
+      "present": presents,
+      "price": prices
     })
   });
-  if(responseAdd.status==401)
-    {
-      console.log('refresh')
-      const responseRef = await fetch("https://localhost:7002//Auth/refreshToken",
-        {
-          method: "PUT",
-          headers:     
-          {
-            'Accept': 'application/json',
-            'Authorization': "Bearer " + sessionStorage.accessToken,
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            WishList:
-              [
-                {
-                  "refreshToken": sessionStorage.accessToken
-                }
-              ]
-          })
-        });
-
-        responseAdd = await fetch(SingUpUrl,
-          {
-            method: "POST",
-            headers:     
-            {
-              'Accept': 'application/json',
-              'Authorization': "Bearer " + sessionStorage.accessToken,
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              WishList:
-                [
-                  {
-                    "present": "string",
-                    "price": 0
-                  }
-                ]
-            })
-          });
-    }
-  if(responseAdd.ok)
-  {
-    console.log(111111)
-  }
+  console.log('resh')
   console.log(responseAdd.status);
+  return responseAdd.status;
 }
 
 function IndexPaje() 
@@ -78,11 +33,10 @@ function IndexPaje()
     e.preventDefault();
     if(sessionStorage.accessToken==null)
     {
-      console.log("adsvfdas");
       return null
     }
   
-    await AddWish('aaaaaaa', 222);
+    const status : number =await AddWish('aaaaaaa', 222);
   }
 
     return (
